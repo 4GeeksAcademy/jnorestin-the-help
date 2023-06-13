9643351d12297e14a58e56191a88307b79462bcc
Jonathan Norestin <jnorestin@gmail.com>
2023-06-13 01:49:09 +0000

    lander

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,43 +2,30 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Navbar } from "../component/navbar";
 
 import VideoComponent from "../component/videoComponent";
-import HelpSlider from '../component/helpslider.js';
-
-
-
-
-
+import HelpSlider from "../component/helpslider";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-<<<<<<< HEAD
-		<div>
-
-			<VideoComponent />
-			<div>
-        <HelpSlider/>
-		 
-=======
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store?.message || "Loading message from the backend (make sure your python backend is running)..."}
->>>>>>> 95b682fea160248afc4f7e1ceea0870be97d5f11
-			</div>
-			<div>
-				<footer />
-			</div>
-
-		</div>
-
-	);
-
+  return (
+    <div>
+      <VideoComponent />
+      <div />
+      <div>
+        <HelpSlider />
+      </div>
+      <div className="text-center mt-5">
+        <h1>Hello Rigo!!</h1>
+        <p>
+          <img src={rigoImageUrl} alt="Rigo" />
+        </p>
+        <div className="alert alert-info">
+          {store?.message || "Loading message from the backend (make sure your python backend is running)..."}
+        </div>
+        <footer></footer>
+      </div>
+    </div>
+  );
 };
