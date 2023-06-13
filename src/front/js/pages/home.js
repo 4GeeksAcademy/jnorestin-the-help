fbad8e782ada4520f849fee5ce615d757fd9ffe4
Jonathan Norestin <jnorestin@gmail.com>
2023-06-13 02:30:31 +0000

    lander

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -17,12 +17,12 @@ export const Home = () => {
         <HelpSlider />
       </div>
       <div className="text-center mt-5">
-        <h1>Hello Rigo!!</h1>
+        <h1>Place holder for profile </h1>
         <p>
           <img src={rigoImageUrl} alt="Rigo" />
         </p>
         <div className="alert alert-info">
-          {store?.message || "Loading message from the backend (make sure your python backend is running)..."}
+          {store?.message || "message will be loaded from backend"}
         </div>
         <footer></footer>
       </div>
