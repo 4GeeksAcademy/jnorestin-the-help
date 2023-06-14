ef2f6b1fddca9da0c2b132323e24e453dbf04a5f
Donald Uzochukwu <121743831+Donducci@users.noreply.github.com>
2023-06-14 14:09:21 +0000

    post apge update

--- src/front/js/pages/post.js
+++ src/front/js/pages/post.js
@@ -1,121 +1,68 @@
-import React, { useState, useEffect, useContext } from "react";
-import "../../../front/styles/help.css";
-import "../../../front/styles/form.css";
-import Lightbox from "react-image-lightbox";
-import "react-image-lightbox/style.css";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import "../../styles/post.css";
 
-export const Post = (props) => {
-  const [lightboxOpen, setLightboxOpen] = useState(false);
-  const [postImageIndex, setPostImageIndex] = useState(0);
-  const [postItem, setPostItem] = useState(0);
-  const [posts, setPosts] = useState([]);
-  const { store, actions } = useContext(Context);
-  useEffect(() => {
-    actions.fetchPosts();
-  }, []);
-  useEffect(() => {
-    if (!store.posts) return;
-    setPosts(store.posts);
-  }, [store.posts]);
-
-  const openLightbox = (index) => {
-    setLightboxOpen(true);
-    setPostImageIndex(index);
-  };
-
-  const closeLightbox = () => {
-    setLightboxOpen(false);
-    setPostImageIndex(0);
-  };
-
-  const createPost = async (postData) => {
-    try {
-      const postWithUser = {
-        description: postData.description,
-        date: new Date().toLocaleString(),
-        location: "Your current location",
-      };
-
-      const response = await fetch(store.apiUrl + "/api/posts", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${store.token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(postWithUser),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to create post");
-      }
-
-      const newPost = await response.json();
-      console.log(">>> 🍎", postData.images);
-      for (const image of postData.images) {
-        let formData = new FormData();
-        formData.append("file", image.file);
-        formData.append("post_id", newPost.id);
-        await actions.createPostImage(formData);
-      }
-      // get posts again -> actions.fetchPosts
-      setPosts((prevPosts) => [...prevPosts, newPost]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleCreatePost = (postData) => {
-    createPost(postData);
-  };
-
-  return (
-    <div>
-      <div className="cards">
-        {posts.map((post, i) => (
-          <div key={post.id} className="card">
+export const Post = () => {
+	const { store, actions } = useContext(Context);
+  console.log(store.posts)
+let request =[
+  {
+   "candidates" :[{"name":"Lee Zee"},{"name":"Dan Bun"}],
+   "date" : "06-02-2023",
+   "description" : "fix my mower",
+   "id": 1,
+   "images": ["https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg","https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg","https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg"],
+   "location": "Huntsville AL",
+	 "user_id": 2
+  },
+  {
+    "candidates" :[{"name":"Lee Zee"},{"name":"Dan Bun"}],
+    "date" : "06-02-2023",
+    "description" : "fix my mower",
+    "id": 1,
+    "images": ["https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg","https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg","https://media.npr.org/assets/img/2011/06/21/flat-698474925749231d3fda07ba360d73772ef39545.jpg"],
+    "location": "Huntsville AL",
+    "user_id": 2
+  },
+]
+	return (
+    <div className="card-container">
+    {request.map((item) => {
+      return (
+        <div className="card">
             <div className="card-body">
               <div className="user-info">
-                <img src={post.user.profile_image} alt="User Profile Image" className="profile_image" />
-                <h5 className="card-title">{post.user.name}</h5>
+                <img src="User Profile Image" className="profile_image" />
+                <h5 className="card-title"></h5>
               </div>
-              <p className="card-text">{post.description}</p>
+              <p className="card-text">{item.description}</p>
               <div className="card-images">
-                {post.images.map((image, index) => (
-                  <div className="image-wrapper" key={image.id}>
-                    <img
-                      src={image.url}
-                      alt="Post Image"
-                      onClick={() => {
-                        openLightbox(index);
-                        setPostItem(i);
-                      }}
-                    />
-                  </div>
-                ))}
+                {item.images.map((image,index) =>{
+                  return (
+                    <img src={image} key={index} className="cardImage"/>
+                  )
+                })}
               </div>
               <div className="post-info">
-                <p className="timestamp">{post.timestamp}</p>
-                <p className="location">{post.location}</p>
+                <p className="date">{item.date}</p>
+                <p className="location">{item.location}</p>
               </div>
-              <a href="#" className="btn btn-primary">
-                HELP ME
-              </a>
             </div>
-          </div>
-        ))}
-      </div>
-      {lightboxOpen && (
-        <Lightbox
-          mainSrc={posts[postItem].images[postImageIndex].url}
-          nextSrc={posts[postItem].images[(postImageIndex + 1) % posts[postItem].images.length].url}
-          prevSrc={posts[postItem].images[(postImageIndex + posts[postItem].images.length - 1) % posts[postItem].images.length].url}
-          onCloseRequest={closeLightbox}
-          onMovePrevRequest={() => setPostImageIndex((postImageIndex + posts[postItem].images.length - 1) % posts[postItem].images.length)}
-          onMoveNextRequest={() => setPostImageIndex((postImageIndex + 1) % posts[postItem].images.length)}
-        />
-      )}
-      
-    </div>
-  );
+            <div>
+              {item.candidates.map((cand,index) =>{
+                return(
+                  <div className="helper-info d-flex navbar">
+                    <img src="helper profile image" className="helper_image mx-5" />
+                    <h5 className="card-text2 mx-5">{cand.name}</h5>
+                    <i class="fa-sharp fa-regular fa-envelope ms-5 me-2"></i>
+                    <i class="fa-solid fa-circle-check mx-3"></i>
+                  </div>
+                )
+              })}
+            </div>
+       </div>
+      )
+    })}
+   </div>
+	);
 };
