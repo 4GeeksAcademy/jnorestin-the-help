967672d73e705754155f949c8e57d1859ee3a450
Ronaldine <121630753+RonaldineP@users.noreply.github.com>
2023-06-16 22:47:03 +0000

    Login signup updates and backend

--- src/api/models.py
+++ src/api/models.py
@@ -11,7 +11,7 @@ class Post(db.Model):
     location = db.Column(db.String(256), unique=False, nullable=False)
     date = db.Column(db.String(256), unique=False, nullable=False)
     images = db.relationship('Image', backref='post')
-    candidates = db.relationship("PostCandidate",backref= "post")
+    candidates = db.relationship("PostCandidate", backref="post")
 
     def __repr__(self):
         return f'<Post {self.id}>'
@@ -20,14 +20,12 @@ class Post(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "description":self.description,
-            "location":self.location,
-            "date":self.date,
-            "user":self.user.serialize(),
-            "images":[image.serialize() for image in self.images],
-            "candidates":[candidate.serialize() for candidate in self.candidates]
-            
-            # do not serialize the password, its a security breach
+            "description": self.description,
+            "location": self.location,
+            "date": self.date,
+            "user": self.user.serialize(),
+            "images": [image.serialize() for image in self.images],
+            "candidates": [candidate.serialize() for candidate in self.candidates]
         }
 
 class User(db.Model):
@@ -36,14 +34,22 @@ class User(db.Model):
     profile_image = db.Column(db.String(256), unique=True)
     password = db.Column(db.String(256), unique=False, nullable=False)
     name = db.Column(db.String(256), unique=False, nullable=False)
+    date_of_birth = db.Column(db.String(256), unique=False, nullable=False)
+    city = db.Column(db.String(256), unique=False, nullable=True)
+    location = db.Column(db.String(256), unique=False, nullable=True)
+    zip_code = db.Column(db.String(256), unique=False, nullable=True)
     helper = db.relationship("Helper", uselist=False, backref="user")
 
     @classmethod
-    def create_user(cls, email, password, name):
+    def create_user(cls, email, password, name, date_of_birth, city, location, zip_code):
         new_user = cls(
             email=email,
             password=password,
-            name=name
+            name=name,
+            date_of_birth=date_of_birth,
+            city=city,
+            location=location,
+            zip_code=zip_code
         )
         db.session.add(new_user)
         try:
@@ -67,13 +73,37 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "profile_image": self.profile_image,
-            "name": self.name
+            "name": self.name,
+            "date_of_birth": self.date_of_birth,
+            "city": self.city,
+            "location": self.location,
+            "zip_code": self.zip_code
         }
+
 
 class PostCandidate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     helper_id = db.Column(db.Integer, ForeignKey("helper.id"))
     post_id = db.Column(db.Integer, ForeignKey("post.id"))
+
+    @classmethod
+    def create_new(cls, helper_id, post_id):
+        helper = Helper.query.get(helper_id)
+        if helper and helper.role == 'Helper':
+            new_post_candidate = cls(
+                helper_id=helper_id,
+                post_id=post_id
+            )
+            db.session.add(new_post_candidate)
+            try:
+                db.session.commit()
+                return new_post_candidate
+            except Exception as error:
+                db.session.rollback()
+                print(error)
+                return None
+        else:
+            return None
 
     def __repr__(self):
         return f'<PostCandidate {self.id}>'
@@ -89,6 +119,7 @@ class Helper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey("user.id"))
     bio = db.Column(db.String(500), unique=False, nullable=False)
+    role = db.Column(db.String(50), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<Helper {self.id}>'
@@ -97,7 +128,8 @@ class Helper(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "bio": self.bio
+            "bio": self.bio,
+            "role": self.role
         }
 
 class Image(db.Model):
@@ -131,6 +163,7 @@ class Image(db.Model):
             "post_id": self.post_id,
             "url": self.url
         }
+
 
     
 
