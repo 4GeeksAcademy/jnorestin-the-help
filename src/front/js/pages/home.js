a783b9a3741c892b111e6427d403dcd497f36e2f
Ronaldine <121630753+RonaldineP@users.noreply.github.com>
2023-06-30 19:50:20 +0000

    title for reviews

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,8 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Navbar } from "../component/navbar";
+
 
 import VideoComponent from "../component/videoComponent";
 import HelpSlider from '../component/helpslider.js';
@@ -19,10 +18,12 @@ export const Home = () => {
 		<div>
 
 			<VideoComponent />
-			<div>
-        <HelpSlider/>
-		 
+			
+			<div className="review-title">
+				<h1> See what they say about us </h1>
 			</div>
+			<HelpSlider />
+
 			<div>
 				<footer />
 			</div>
